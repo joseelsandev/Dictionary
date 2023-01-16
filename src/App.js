9307b1d330604d2a2e84e83251dd59ca3cd90274
joseelsandev <97639604+joseelsandev@users.noreply.github.com>
2023-01-16 19:16:01 -0400

    useffect was added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 
 const App = () => {
+
+  const URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+
+  const [word, setWord] = useState("")
+
+
+  const getWord = (word) => {
+    const response = fetch(`${URL}`);
+
+
+  }
+
+  useEffect(() => {
+    getWord(word)
+  }, [word])
+
   return (
     <div className="App">
       <header className="header">
@@ -10,6 +28,7 @@ const App = () => {
       </header>
       <div className='container'>
         <input type='text' />
+        
       </div>
 
     </div>
