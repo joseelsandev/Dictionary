78288d37f72fb6f09f4c40c42258e85d95d4cd85
joseelsandev <97639604+joseelsandev@users.noreply.github.com>
2023-01-16 20:44:40 -0400

    end of day

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
+import { FaSearch } from 'react-icons/fa';
 
 
 const App = () => {
 
   const URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 
-  const [word, setWord] = useState("")
+  const [word, setWord] = useState("a")
 
 
-  const getWord = (word) => {
-    const response = fetch(`${URL}`);
-
+  const getWord = async (word) => {
+    console.log({ curentURL: `${URL}${word}` });
+    const response = await fetch(`${URL}${word}`);
+    const data = await response.json();
+    console.log(data);
+    return data
 
   }
 
@@ -27,8 +30,24 @@ const App = () => {
 
       </header>
       <div className='container'>
-        <input type='text' />
-        
+        {/* <input type='text' /> */}
+
+        {/* SEARCH BAR  */}
+        {/* https://mdbootstrap.com/docs/standard/forms/search/ */}
+
+        {/* https://react-icons.github.io/react-icons/icons?name=fa */}
+        <div className="input-group">
+          <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" onChange={(e) => setWord(e.target.value)} />
+          <button type="button" className="btn btn-outline-primary">
+            <FaSearch />
+          </button>
+        </div>
+
+        <h1>{word}</h1>
+
+
+
+
       </div>
 
     </div>
